6bac10e95058c59c94c0578265bd87da79501d80
adi <adityavshedge3101@gmail.com>
2024-02-22 12:38:52 +0530

    [LM-28]: added a greeting controller with root endpoint

---
+++ src/main/java/com/technogise/leavemanagementbe/LeaveManagementBeApplication.java
@@ -0,0 +1,13 @@
+package com.technogise.leavemanagementbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveManagementBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveManagementBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technogise/leavemanagementbe/LeaveManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technogise.leavemanagementbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveManagementBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
