d12acf7bb4b490428d9837a3ed0736258e1d9299
adi <adityavshedge3101@gmail.com>
2024-02-23 17:16:40 +0530

    [LM-28]: updated healthCheck to return an 'ok' status

--- src/main/java/com/technogise/leavemanagement/controllers/HealthController.java
+++ src/main/java/com/technogise/leavemanagement/controllers/HealthController.java
@@ -1,14 +1,16 @@
 package com.technogise.leavemanagement.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HealthController {
 
     @GetMapping("/")
-    public String healthCheck() {
-        return "Hello World !";
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
     }
 
 }
