553fc335637d7458c05360ced5f59ff59a5d65d3
adi <adityavshedge3101@gmail.com>
2024-02-23 16:22:11 +0530

    [LM-28]: updated greetUser to healthCheck

--- src/main/java/com/technogise/leavemanagement/controllers/HealthController.java
+++ src/main/java/com/technogise/leavemanagement/controllers/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HealthController {
 
     @GetMapping("/")
-    public String greetUser() {
+    public String healthCheck() {
         return "Hello World !";
     }
 
