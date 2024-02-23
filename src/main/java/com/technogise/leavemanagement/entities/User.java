b72f39be549b6f86376546ce3c4625631650ebe6
Shinichi <rishabhatre@gmail.com>
2024-02-23 11:52:08 +0530

    [LM-33]: added attributes for user entity

--- src/main/java/com/technogise/leavemanagement/entities/User.java
+++ src/main/java/com/technogise/leavemanagement/entities/User.java
@@ -1,8 +1,22 @@
 package com.technogise.leavemanagement.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String[] roles;
 }
