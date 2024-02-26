c1c5b1479729f60862ddddaf825c7f9f93af812e
adi <adityavshedge3101@gmail.com>
2024-02-26 14:11:37 +0530

    [LM-28]: updated mapping from root to health-check

--- src/main/java/com/technogise/leavemanagement/controllers/HealthController.java
+++ src/main/java/com/technogise/leavemanagement/controllers/HealthController.java
@@ -1,14 +1,13 @@
 package com.technogise.leavemanagement.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HealthController {
 
-    @GetMapping("/")
+    @GetMapping("/health-check")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("OK");
     }
