c1c326c52bba7792c9cfe1482e205bc3d1c0f74e
adi <adityavshedge3101@gmail.com>
2024-02-28 15:35:28 +0530

    [LM-7]: added leave dto

---
+++ src/main/java/com/technogise/leavemanagement/dtos/LeaveDTO.java
@@ -0,0 +1,15 @@
+package com.technogise.leavemanagement.dtos;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class LeaveDTO {
+
+    private Long userId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String leaveType;
+    private String description;
+
+}
