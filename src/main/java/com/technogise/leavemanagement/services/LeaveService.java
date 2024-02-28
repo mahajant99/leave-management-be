af6c010e9a3dedee8ddfe37eda545e7955f48c6b
adi <adityavshedge3101@gmail.com>
2024-02-28 15:46:35 +0530

    [LM-7]: added createOneDayLeave to leave service

--- src/main/java/com/technogise/leavemanagement/services/LeaveService.java
+++ src/main/java/com/technogise/leavemanagement/services/LeaveService.java
@@ -1,14 +1,20 @@
 package com.technogise.leavemanagement.services;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import com.technogise.leavemanagement.dtos.LeaveDTO;
+import com.technogise.leavemanagement.entities.HalfDay;
 import com.technogise.leavemanagement.entities.Leave;
+import com.technogise.leavemanagement.entities.User;
 import com.technogise.leavemanagement.repositories.LeaveRepository;
+import com.technogise.leavemanagement.repositories.UserRepository;
 
 @Service
 public class LeaveService {
@@ -16,13 +22,32 @@ public class LeaveService {
     @Autowired
     private LeaveRepository leaveRepository;
 
-    public Page<Leave> getLeavesByUserId(Long userId, int page, int size) {
+    @Autowired
+    private UserRepository userRepository;
 
-        Sort sort = Sort.by(Sort.Direction.DESC, "date");
+    private User user;
 
-        Pageable pageable = PageRequest.of(page, size, sort);
+    public Leave createOneDayLeave(LeaveDTO leaveDTO) {
 
-        return leaveRepository.findByUserId(userId, pageable);
+        double duration;
+        HalfDay halfDay;
+
+        if (leaveDTO.getLeaveType() == "fullday") {
+            duration = 1;
+            return new Leave(null, leaveDTO.getStartDate(), duration, leaveDTO.getDescription(), null, user);
+
+        } else {
+            duration = 0.5;
+        }
+
+        if (leaveDTO.getLeaveType().equals("first half")) {
+            halfDay = HalfDay.FIRST_HALF;
+        } else {
+            halfDay = HalfDay.SECOND_HALF;
+        }
+
+        return new Leave(null, leaveDTO.getStartDate(), duration, leaveDTO.getDescription(), halfDay,
+                user);
     }
 
 }
