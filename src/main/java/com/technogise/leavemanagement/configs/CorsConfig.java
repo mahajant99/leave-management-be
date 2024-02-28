1073923570e5f70add70afb0dc267fa7e01871f5
Tushar Mahajan <mahajant99@gmail.com>
2024-02-28 15:27:22 +0530

    [LM-32]: Refactor Cors Config for allowed origins
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- src/main/java/com/technogise/leavemanagement/configs/CorsConfig.java
+++ src/main/java/com/technogise/leavemanagement/configs/CorsConfig.java
@@ -1,20 +1,19 @@
 package com.technogise.leavemanagement.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("production")
-public class ProductionCorsConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${app.allowedOrigin.${spring.profiles.active}}")
+    private String allowedOrigin;
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://leave-management-fe.onrender.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins(allowedOrigin);
     }
 }
