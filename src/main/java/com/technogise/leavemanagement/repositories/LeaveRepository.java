82630206bf620b2368d67d42bd6deefb10267dbc
Shinichi <rishabhatre@gmail.com>
2024-02-29 13:03:16 +0530

    [LM-18]: Added findByUserId function with query to get leaves of specific userId in desc order by date.

--- src/main/java/com/technogise/leavemanagement/repositories/LeaveRepository.java
+++ src/main/java/com/technogise/leavemanagement/repositories/LeaveRepository.java
@@ -1,9 +1,16 @@
 package com.technogise.leavemanagement.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.technogise.leavemanagement.entities.Leave;
 
 public interface LeaveRepository extends JpaRepository<Leave, Long> {
+
+    @Query("SELECT leaveRequest FROM Leave leaveRequest WHERE leaveRequest.user.id = :userId ORDER BY leaveRequest.date DESC")
+    Page<Leave> findByUserId(Long userId, Pageable pageable);
+
 
 }
