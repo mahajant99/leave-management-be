b14d91e4c791ff5279bcb443be4eb26acdc4e68c
Shinichi <rishabhatre@gmail.com>
2024-02-29 12:27:08 +0530

    [LM-18]: Added LeaveService

---
+++ src/main/java/com/technogise/leavemanagement/services/LeaveService.java
@@ -0,0 +1,14 @@
+package com.technogise.leavemanagement.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.technogise.leavemanagement.repositories.LeaveRepository;
+
+public class LeaveService {
+
+    @Autowired
+    private LeaveRepository leaveRepository;
+
+    
+    
+}
