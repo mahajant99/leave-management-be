3cb9248c0936f476ac60f345ddcff7830f1c89d9
Shinichi <rishabhatre@gmail.com>
2024-02-29 14:25:10 +0530

    [LM-18]: Added getLeavesByUserId method in LeaveService.

--- src/main/java/com/technogise/leavemanagement/services/LeaveService.java
+++ src/main/java/com/technogise/leavemanagement/services/LeaveService.java
@@ -1,7 +1,12 @@
 package com.technogise.leavemanagement.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import com.technogise.leavemanagement.entities.Leave;
 import com.technogise.leavemanagement.repositories.LeaveRepository;
 
 public class LeaveService {
@@ -9,6 +14,13 @@ public class LeaveService {
     @Autowired
     private LeaveRepository leaveRepository;
 
-    
-    
+    public Page<Leave> getLeavesByUserId(Long userId, int page, int size) {
+
+        Sort sort = Sort.by(Sort.Direction.DESC, "date");
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return leaveRepository.findByUserId(userId, pageable);
+    }
+
 }
