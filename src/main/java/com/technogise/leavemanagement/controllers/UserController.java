83eff9801bb6a098309aa57be4b2cbaa1701b9ca
Shinichi <rishabhatre@gmail.com>
2024-02-29 16:53:53 +0530

    [LM-18]: Created retrieveLeaves method in controller.

--- src/main/java/com/technogise/leavemanagement/controllers/UserController.java
+++ src/main/java/com/technogise/leavemanagement/controllers/UserController.java
@@ -1,5 +1,33 @@
 package com.technogise.leavemanagement.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.technogise.leavemanagement.entities.Leave;
+import org.springframework.data.domain.Page;
+
+import com.technogise.leavemanagement.services.LeaveService;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    
+
+    @Autowired
+    private LeaveService leaveService;
+
+    @GetMapping("/{userId}/leaves")
+    public ResponseEntity<Page<Leave>> retrieveLeaves(@PathVariable("userId") Long userId, @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "6") int size) {
+
+        Page<Leave> leavesPage = leaveService.getLeavesByUserId(userId, page, size);
+        return leavesPage.isEmpty() ? new ResponseEntity<Page<Leave>>(HttpStatus.NO_CONTENT)
+                : new ResponseEntity<Page<Leave>>(leavesPage, HttpStatus.OK);
+
+    }
+
 }
