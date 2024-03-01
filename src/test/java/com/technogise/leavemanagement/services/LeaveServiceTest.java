18e5f8cd201f52039b6b00f60f7a0c809f9c45f1
adi <adityavshedge3101@gmail.com>
2024-03-01 16:21:12 +0530

    [LM-7]: added test for checking full day leave

--- src/test/java/com/technogise/leavemanagement/services/LeaveServiceTest.java
+++ src/test/java/com/technogise/leavemanagement/services/LeaveServiceTest.java
@@ -1,17 +1,14 @@
 package com.technogise.leavemanagement.services;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import java.util.Optional;
-
+import com.technogise.leavemanagement.dtos.LeaveDTO;
+import com.technogise.leavemanagement.entities.Leave;
 import com.technogise.leavemanagement.entities.User;
 import com.technogise.leavemanagement.repositories.UserRepository;
 
@@ -23,5 +20,19 @@ public class LeaveServiceTest {
 
     @InjectMocks
     private LeaveService leaveService;
+
+    @Test
+    public void Should_HaveHalfDayNull_When_LeaveTypeIsFullDay() {
+        User user = new User();
+        user.setId(1L);
+
+        LeaveDTO leaveDTO = new LeaveDTO();
+        leaveDTO.setUserId(1L);
+        leaveDTO.setLeaveType("fullday");
+
+        Leave leave = leaveService.createOneDayLeave(leaveDTO, user);
+
+        assertNull(leave.getHalfDay());        
+    }
 
 }
