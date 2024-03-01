c37379d2b6e5a4063700e39232aa9707584d699a
Allay Sahoo <allaysahoo@gmail.com>
2024-03-01 12:04:03 +0530

    [LM-19]: Added leave controller to delete leave.

---
+++ src/main/java/com/technogise/leavemanagement/controllers/LeaveContoller.java
@@ -0,0 +1,24 @@
+package com.technogise.leavemanagement.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.technogise.leavemanagement.services.LeaveService;
+
+@RestController
+public class LeaveContoller {
+
+    @Autowired
+    private LeaveService leaveService;
+
+    @DeleteMapping("/leaves/{id}")
+
+    public ResponseEntity<Void> deleteLeave(@PathVariable Long id) {
+        leaveService.remove(id);
+        return ResponseEntity.noContent().build();
+
+    }
+}
