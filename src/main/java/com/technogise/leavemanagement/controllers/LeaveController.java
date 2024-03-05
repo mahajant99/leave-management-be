c97c3966cf10740bd404ff6f45ce6573b9fcf539
Allay Sahoo <allaysahoo@gmail.com>
2024-03-05 11:16:12 +0530

    [LM-20]: fixed typo for controller spelling.

--- src/main/java/com/technogise/leavemanagement/controllers/LeaveController.java
+++ src/main/java/com/technogise/leavemanagement/controllers/LeaveController.java
@@ -3,6 +3,7 @@ package com.technogise.leavemanagement.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +28,12 @@ public class LeaveController {
         Page<Leave> leavesPage = leaveService.getLeavesByUserId(userId, page, size);
         return leavesPage.isEmpty() ? new ResponseEntity<Page<Leave>>(HttpStatus.NO_CONTENT)
                 : new ResponseEntity<Page<Leave>>(leavesPage, HttpStatus.OK);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLeave(@PathVariable("id") Long id) {
+        leaveService.remove(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
