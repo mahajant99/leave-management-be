e681ae67590e56ebeab66927b1b9ff54377eae63
adi <adityavshedge3101@gmail.com>
2024-03-05 11:12:40 +0530

    [LM-7]: fixed and simplified addLeaves

--- src/main/java/com/technogise/leavemanagement/controllers/LeaveController.java
+++ src/main/java/com/technogise/leavemanagement/controllers/LeaveController.java
@@ -1,7 +1,6 @@
 package com.technogise.leavemanagement.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.technogise.leavemanagement.dtos.LeaveDTO;
 import com.technogise.leavemanagement.entities.Leave;
 import com.technogise.leavemanagement.services.LeaveService;
@@ -22,15 +20,11 @@ public class LeaveController {
 
     @PostMapping
     public ResponseEntity<List<Leave>> addLeaves(@RequestBody LeaveDTO leaveDTO) {
+        if(!validateLeaveDTO(leaveDTO)) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
         List<Leave> leaves = leaveService.addLeaves(leaveDTO);
 
-        if(!validateLeaveDTO(leaveDTO)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-
-        if (leaves.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        if (leaves.isEmpty()) return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 
         return ResponseEntity.status(HttpStatus.CREATED).body(leaves);
     }
