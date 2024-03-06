2d52ab7374d873d73f5d307d2bc477f1c7f92133
adi <adityavshedge3101@gmail.com>
2024-03-06 11:32:48 +0530

    [LM-7]: updated LeaveDTO, added validation using annotations

--- src/main/java/com/technogise/leavemanagement/dtos/LeaveDTO.java
+++ src/main/java/com/technogise/leavemanagement/dtos/LeaveDTO.java
@@ -1,15 +1,29 @@
 package com.technogise.leavemanagement.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
 
 @Data
 public class LeaveDTO {
 
+    @NotNull(message = "ID is required")
     private Long userId;
+
+    @NotNull(message = "Start Date is required")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+
+    @NotNull(message = "End Date is required")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
+
+    @NotBlank(message = "Leave Type is required")
     private String leaveType;
+
+    @NotBlank(message = "Description is required")
     private String description;
 
 }
