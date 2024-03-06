baf70c434063a780f8d1e2f0792a5a8a9fb2b72f
adi <adityavshedge3101@gmail.com>
2024-03-06 11:15:57 +0530

    [LM-7]: simplified leave controller, also removed validateLeaveDTO

--- src/main/java/com/technogise/leavemanagement/controllers/LeaveController.java
+++ src/main/java/com/technogise/leavemanagement/controllers/LeaveController.java
@@ -1,6 +1,7 @@
 package com.technogise.leavemanagement.controllers;
 
 import java.util.List;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,22 +20,11 @@ public class LeaveController {
     private LeaveService leaveService;
 
     @PostMapping
-    public ResponseEntity<List<Leave>> addLeaves(@RequestBody LeaveDTO leaveDTO) {
-        if(!validateLeaveDTO(leaveDTO)) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-
+    public ResponseEntity<List<Leave>> addLeaves(@Valid @RequestBody LeaveDTO leaveDTO) {
         List<Leave> leaves = leaveService.addLeaves(leaveDTO);
 
-        if (leaves.isEmpty()) return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(leaves);
+        return leaves.isEmpty() ?
+                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build() :
+                ResponseEntity.status(HttpStatus.CREATED).body(leaves);
     }
-
-    private boolean validateLeaveDTO(LeaveDTO leaveDTO) {
-        return (leaveDTO.getUserId() == null ||
-                leaveDTO.getStartDate() == null ||
-                leaveDTO.getEndDate() == null ||
-                leaveDTO.getLeaveType() == null ||
-                leaveDTO.getLeaveType().isEmpty());
-    }
-
 }
