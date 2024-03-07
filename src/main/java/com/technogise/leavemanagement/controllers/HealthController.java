c2b60db1483a94676f51debab258e3acf514a058
Allay Sahoo <allaysahoo@gmail.com>
2024-03-07 13:03:29 +0530

    [LM-19]: added CORS annotation to controllers.

--- src/main/java/com/technogise/leavemanagement/controllers/HealthController.java
+++ src/main/java/com/technogise/leavemanagement/controllers/HealthController.java
@@ -2,9 +2,11 @@ package com.technogise.leavemanagement.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class HealthController {
 
     @GetMapping("/health-check")
