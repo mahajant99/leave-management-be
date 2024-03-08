3b3b33a48c7783d126f2a7a81d511d90cd2224d3
Allay Sahoo <allaysahoo@gmail.com>
2024-03-08 15:49:12 +0530

    [LM-19]: removed update query from leave repository.

--- src/main/java/com/technogise/leavemanagement/repositories/LeaveRepository.java
+++ src/main/java/com/technogise/leavemanagement/repositories/LeaveRepository.java
@@ -1,6 +1,5 @@
 package com.technogise.leavemanagement.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
@@ -12,8 +11,4 @@ public interface LeaveRepository extends JpaRepository<Leave, Long> {
 
     @Query("SELECT leaveRequest FROM Leave leaveRequest WHERE leaveRequest.user.id = :userId ORDER BY leaveRequest.date DESC")
     Page<Leave> findByUserId(Long userId, Pageable pageable);
-    
-    @Modifying(flushAutomatically = true)
-    @Query("UPDATE Leave l SET l.deleted = true WHERE l.id=?1")
-    void softDeleteById(Long id);
 }
