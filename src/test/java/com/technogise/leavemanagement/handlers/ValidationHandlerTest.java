4f01064bcdb5944f3d82e651629742fa0e097c71
adi <adityavshedge3101@gmail.com>
2024-03-08 11:06:09 +0530

    [LM-7]: removed unused imports

--- src/test/java/com/technogise/leavemanagement/handlers/ValidationHandlerTest.java
+++ src/test/java/com/technogise/leavemanagement/handlers/ValidationHandlerTest.java
@@ -9,16 +9,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
