0206f7f1f2450bcc62fca89615fdd615f00555f3
Allay Sahoo <allaysahoo@gmail.com>
2024-03-08 13:38:44 +0530

    [LM-19]: Resolve rebase conflicts

--- src/main/java/com/technogise/leavemanagement/services/LeaveService.java
+++ src/main/java/com/technogise/leavemanagement/services/LeaveService.java
@@ -1,23 +1,14 @@
 package com.technogise.leavemanagement.services;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import com.technogise.leavemanagement.dtos.LeaveDTO;
-import com.technogise.leavemanagement.entities.HalfDay;
 import com.technogise.leavemanagement.entities.Leave;
-import com.technogise.leavemanagement.entities.User;
 import com.technogise.leavemanagement.repositories.LeaveRepository;
-import com.technogise.leavemanagement.repositories.UserRepository;
-
 import jakarta.persistence.EntityNotFoundException;
-
 
 @Service
 @Transactional
@@ -26,58 +17,10 @@ public class LeaveService {
     @Autowired
     private LeaveRepository leaveRepository;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    private User user;
-
-    public User getUserById(Long id) {
-        Optional<User> optionalUser = userRepository.findById(id);
-
-        if (optionalUser.isPresent()) {
-            user = optionalUser.get();
-        }
-
-        return user;
-    }
-
-    public Leave createOneDayLeave(LeaveDTO leaveDTO) {
-
-        double duration;
-        HalfDay halfDay;
-
-        user = getUserById(leaveDTO.getUserId());
-
-        if (leaveDTO.getLeaveType() == "fullday") {
-            duration = 1;
-            return new Leave(null, leaveDTO.getStartDate(), duration, leaveDTO.getDescription(), null, user, false);
-
-        } else {
-            duration = 0.5;
-        }
-
-        if (leaveDTO.getLeaveType().equals("first half")) {
-            halfDay = HalfDay.FIRST_HALF;
-        } else {
-            halfDay = HalfDay.SECOND_HALF;
-        }
-
-        return new Leave(null, leaveDTO.getStartDate(), duration, leaveDTO.getDescription(), halfDay,
-                user, false);
-    }
-
-    public List<Leave> addLeave(LeaveDTO leaveDTO) {
-
-        List<Leave> addedLeaves = new ArrayList<>();
-
-        if (leaveDTO.getStartDate().equals(leaveDTO.getEndDate())) {
-            Leave leave = createOneDayLeave(leaveDTO);
-            leaveRepository.save(leave);
-            addedLeaves.add(leave);
-        }
-
-        return addedLeaves;
-
+    public Page<Leave> getLeavesByUserId(Long userId, int page, int size) {
+        Sort sort = Sort.by(Sort.Direction.DESC, "date");
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return leaveRepository.findByUserId(userId, pageable);
     }
 
     @SuppressWarnings("null")
@@ -87,5 +30,4 @@ public class LeaveService {
         leave.setDeleted(true);
         leaveRepository.save(leave);
     }
-
 }
