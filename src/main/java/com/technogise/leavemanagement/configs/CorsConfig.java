dd6ee882edcc333491e8163809bcd8ccb150c2c0
Tushar Mahajan <mahajant99@gmail.com>
2024-03-11 13:21:08 +0530

    [LM-19]: Allow methods in cors config
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- src/main/java/com/technogise/leavemanagement/configs/CorsConfig.java
+++ src/main/java/com/technogise/leavemanagement/configs/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigin);
+                .allowedOrigins(allowedOrigin)
+                .allowedMethods("GET", "POST", "DELETE");
     }
 }
