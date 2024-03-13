07cd5af03d8f606503e6dfc9b5412cd77bd7ccd1
adi <adityavshedge3101@gmail.com>
2024-03-13 09:52:44 +0530

    [LM-7]: added error response dto

---
+++ src/main/java/com/technogise/leavemanagement/dtos/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.technogise.leavemanagement.dtos;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private int statusCode;
+    private String message;
+}
