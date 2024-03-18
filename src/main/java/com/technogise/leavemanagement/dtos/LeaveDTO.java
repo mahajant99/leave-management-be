12e97adc6c07a4a318ef3079735633c70087d561
adi <adityavshedge3101@gmail.com>
2024-03-18 17:42:55 +0530

    [LM-58]: updated leavedto to support builder pattern

--- src/main/java/com/technogise/leavemanagement/dtos/LeaveDTO.java
+++ src/main/java/com/technogise/leavemanagement/dtos/LeaveDTO.java
@@ -2,11 +2,13 @@ package com.technogise.leavemanagement.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class LeaveDTO {
 
     @NotNull(message = "ID is required")
