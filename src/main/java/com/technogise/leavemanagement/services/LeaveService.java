b202526d52098de53ea6ff9f3b65bcb47fd17d7c
adi <adityavshedge3101@gmail.com>
2024-03-18 13:40:30 +0530

    [LM-58]: removed unused import

--- src/main/java/com/technogise/leavemanagement/services/LeaveService.java
+++ src/main/java/com/technogise/leavemanagement/services/LeaveService.java
@@ -1,7 +1,6 @@
 package com.technogise.leavemanagement.services;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
