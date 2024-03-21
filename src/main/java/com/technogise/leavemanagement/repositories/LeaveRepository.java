1a862876107db7975718ffa294c58a550220ef18
adi <adityavshedge3101@gmail.com>
2024-03-21 16:45:03 +0530

    [LM-69]: added method to find existing leaves by user

--- src/main/java/com/technogise/leavemanagement/repositories/LeaveRepository.java
+++ src/main/java/com/technogise/leavemanagement/repositories/LeaveRepository.java
@@ -1,14 +1,19 @@
 package com.technogise.leavemanagement.repositories;
 
+import com.technogise.leavemanagement.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import com.technogise.leavemanagement.entities.Leave;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
 public interface LeaveRepository extends JpaRepository<Leave, Long> {
 
     Page<Leave> findByUserIdAndDeletedFalseOrderByDateDesc(Long userId, Pageable pageable);
 
+    Optional<Leave> findByUserAndDate(User user, LocalDate date);
 }
