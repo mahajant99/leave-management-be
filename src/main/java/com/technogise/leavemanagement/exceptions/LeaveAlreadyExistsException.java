769a60f01ddedb945cec6bc209deb4646a2e21b0
adi <adityavshedge3101@gmail.com>
2024-03-22 12:16:06 +0530

    [LM-69]: updated message for leave already exists exception

--- src/main/java/com/technogise/leavemanagement/exceptions/LeaveAlreadyExistsException.java
+++ src/main/java/com/technogise/leavemanagement/exceptions/LeaveAlreadyExistsException.java
@@ -3,7 +3,7 @@ package com.technogise.leavemanagement.exceptions;
 import java.time.LocalDate;
 
 public class LeaveAlreadyExistsException extends Exception{
-    public LeaveAlreadyExistsException(LocalDate date) {
-        super("Leave exists for: " + date);
+    public LeaveAlreadyExistsException() {
+        super("Leave already exists");
     }
 }
