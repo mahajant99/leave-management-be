1b93e10f6b929a9b206c9315c0ccd49850776f34
adi <adityavshedge3101@gmail.com>
2024-03-26 09:44:59 +0530

    [LM-69]: removed unused imports and fixed formatting in leave repo

--- src/main/java/com/technogise/leavemanagement/repositories/LeaveRepository.java
+++ src/main/java/com/technogise/leavemanagement/repositories/LeaveRepository.java
@@ -1,6 +1,5 @@
 package com.technogise.leavemanagement.repositories;
 
-import com.technogise.leavemanagement.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
@@ -8,11 +7,11 @@ import org.springframework.data.domain.Pageable;
 import com.technogise.leavemanagement.entities.Leave;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 @Repository
 public interface LeaveRepository extends JpaRepository<Leave, Long> {
 
     Page<Leave> findByUserIdAndDeletedFalseOrderByDateDesc(Long userId, Pageable pageable);
 
-    boolean existsByUserIdAndDateAndDeletedFalse(Long userId, LocalDate date);}
+    boolean existsByUserIdAndDateAndDeletedFalse(Long userId, LocalDate date);
+}
