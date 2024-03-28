7b48f587e7fccc6c765ab6ccce35a2f91342f676
Shinichi <rishabhatre@gmail.com>
2024-03-28 13:48:15 +0530

    [LM-79]: Added  tests.

--- src/main/java/com/technogise/leavemanagement/services/GoogleCalendarService.java
+++ src/main/java/com/technogise/leavemanagement/services/GoogleCalendarService.java
@@ -23,7 +23,7 @@ import java.time.ZoneId;
 public class GoogleCalendarService {
 
     private static final String CALENDAR_ID = "abf2ff4f7bcc921456b336e2a7dc80b3daa936d455cdbcebdbc8e24473a8c175@group.calendar.google.com";
-    private static final String SERVICE_ACCOUNT_KEY_FILE_PATH = "src\\main\\resources\\service-account-key.json";
+    static final String SERVICE_ACCOUNT_KEY_FILE_PATH = "src\\main\\resources\\service-account-key.json";
     private static final List<String> CALENDAR_SCOPES = Arrays.asList("https://www.googleapis.com/auth/calendar");
     private static final String APPLICATION_NAME = "LeaveManagement";
     private static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd";
@@ -82,6 +82,6 @@ public class GoogleCalendarService {
         } catch (IOException e) {
             
             e.printStackTrace();
-        }
+        }    
     }
 }
--- src/test/java/com/technogise/leavemanagement/services/GoogleCalendarServiceTest.java
+++ src/test/java/com/technogise/leavemanagement/services/GoogleCalendarServiceTest.java
@@ -1,4 +1,8 @@
 package com.technogise.leavemanagement.services;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.model.Event;
 import com.technogise.leavemanagement.entities.Leave;
@@ -7,23 +11,32 @@ import com.technogise.leavemanagement.enums.HalfDay;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 public class GoogleCalendarServiceTest {
 
-    private GoogleCalendarService googleCalendarService;
+   @Mock
     private Calendar mockCalendarService;
+
+    private GoogleCalendarService googleCalendarService;
+    static final String SERVICE_ACCOUNT_KEY_FILE_PATH = "src\\main\\resources\\service-account-key.json";
+    private static final List<String> CALENDAR_SCOPES = Arrays.asList("https://www.googleapis.com/auth/calendar");
+    private static final String APPLICATION_NAME = "LeaveManagement";
 
     @BeforeEach
     public void setUp() throws GeneralSecurityException, IOException {
+        MockitoAnnotations.openMocks(this);
         googleCalendarService = new GoogleCalendarService();
-        mockCalendarService = mock(Calendar.class);
         googleCalendarService.initializeCalendarService(); 
     }
 
@@ -56,6 +69,27 @@ public class GoogleCalendarServiceTest {
         Event event = googleCalendarService.createLeaveEvent(leave);
         assertNotNull(event, "Event should not be null");
         assertTrue(event.getSummary().contains(" on leave(FIRSTHALF)"), "Rick on leave(FIRSTHALF)");
+    }
+    
+     @Test
+    public void testInitializeCalendarServiceSuccess() throws IOException, GeneralSecurityException {
+        Calendar service = initializeCalendarService();
+        assertNotNull(service, "Calendar service should not be null");
+    }
+
+    public Calendar initializeCalendarService() {
+        GoogleCredential credential = null;
+        Calendar service = null;
+        try {
+            credential = GoogleCredential.fromStream(new FileInputStream(SERVICE_ACCOUNT_KEY_FILE_PATH))
+                    .createScoped(CALENDAR_SCOPES);
+            service = new Calendar.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), credential)
+                    .setApplicationName(APPLICATION_NAME)
+                    .build();
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+        return service;
     }
     
 }
