3d125d1b09fc58fd5955b3ef032b175f5072c2e1
Tushar Mahajan <mahajant99@gmail.com>
2024-03-31 23:37:14 +0530

    [LM-75]: Fix PathRequest cannot be resolved
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- src/main/java/com/technogise/leavemanagement/configs/SecurityConfig.java
+++ src/main/java/com/technogise/leavemanagement/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.technogise.leavemanagement.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
