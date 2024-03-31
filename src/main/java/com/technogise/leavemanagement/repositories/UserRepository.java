5cc35671b804094e3585e7607c497e3a04d1bd1c
Tushar Mahajan <mahajant99@gmail.com>
2024-03-31 16:45:39 +0530

    [LM-75]: Update UserRepository
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- src/main/java/com/technogise/leavemanagement/repositories/UserRepository.java
+++ src/main/java/com/technogise/leavemanagement/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.technogise.leavemanagement.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.technogise.leavemanagement.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
