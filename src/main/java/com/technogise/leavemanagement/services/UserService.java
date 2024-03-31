b93592df4f8327d3e532d7db0b76240b5325a5b1
Tushar Mahajan <mahajant99@gmail.com>
2024-03-31 23:45:24 +0530

    [LM-75]: Implement UserService with Google ID token verification
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

---
+++ src/main/java/com/technogise/leavemanagement/services/UserService.java
@@ -0,0 +1,38 @@
+package com.technogise.leavemanagement.services;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.technogise.leavemanagement.configs.JWTUtils;
+import com.technogise.leavemanagement.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+    
+    @Autowired
+    private final JWTUtils jwtUtils;
+
+    @Autowired
+    private final GoogleIdTokenVerifier verifier;
+
+    public UserService(@Value("${app.googleClientId}") String clientId, UserRepository userRepository,
+                          JWTUtils jwtUtils) {
+        this.userRepository = userRepository;
+        this.jwtUtils = jwtUtils;
+        NetHttpTransport transport = new NetHttpTransport();
+        JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+        verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                .setAudience(Collections.singletonList(clientId))
+                .build();
+    }
+}
