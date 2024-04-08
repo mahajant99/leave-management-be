d00dacb59870e774d4f8a595c1665adc41a14ff8
Shinichi <rishabhatre@gmail.com>
2024-04-08 17:17:11 +0530

    [LM-75]:Removed userIdNotFound exception.

--- src/test/java/com/technogise/leavemanagement/handlers/GlobalExceptionHandlerTest.java
+++ src/test/java/com/technogise/leavemanagement/handlers/GlobalExceptionHandlerTest.java
@@ -1,10 +1,6 @@
 package com.technogise.leavemanagement.handlers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import com.technogise.leavemanagement.enums.LeaveType;
 import com.technogise.leavemanagement.exceptions.LeaveAlreadyExistsException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,16 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import java.time.LocalDate;
 
 import com.technogise.leavemanagement.dtos.ErrorResponse;
 import com.technogise.leavemanagement.exceptions.LeaveNotFoundException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.technogise.leavemanagement.dtos.LeaveDTO;
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -33,9 +22,6 @@ public class GlobalExceptionHandlerTest {
 
     @Autowired
     private GlobalExceptionHandler globalExceptionHandler;
-
-    @Autowired
-    private MockMvc mockMvc;
 
     @Test
     @DisplayName(value = "Given a leave does not exists , when Leave not found exception rises , then should return NOT_FOUND status.")
@@ -50,30 +36,6 @@ public class GlobalExceptionHandlerTest {
                 .handleLeaveNotFoundException(new LeaveNotFoundException(nonExistentId));
 
         assertEquals(errorResponse.getStatusCode(), responseEntity.getStatusCode().value());
-    }
-
-    @Test
-    public void Should_ReturnUserNotFound_When_UserIdIsInvalid() throws Exception {
-        LeaveDTO leaveDTO = LeaveDTO.builder()
-                .startDate(LocalDate.of(2024, 3, 16))
-                .endDate(LocalDate.of(2024, 3, 17))
-                .description("Vacation")
-                .userId(12L)
-                .leaveType(String.valueOf(LeaveType.FULLDAY))
-                .build();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        String requestBody = objectMapper.writeValueAsString(leaveDTO);
-
-        mockMvc.perform(post("/leaves")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isNotFound())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.statusCode").value(HttpStatus.NOT_FOUND.value()))
-                .andExpect(jsonPath("$.message").value("User not found: " + leaveDTO.getUserId()));
-
     }
 
     @Test
