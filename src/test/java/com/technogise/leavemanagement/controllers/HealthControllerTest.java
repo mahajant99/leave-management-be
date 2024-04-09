23d7acc5308809649ec795b6bc73ee3844aeb02a
Shinichi <rishabhatre@gmail.com>
2024-04-09 06:20:58 +0530

    [LM-75]:Updated healthcontroller test.

--- src/test/java/com/technogise/leavemanagement/controllers/HealthControllerTest.java
+++ src/test/java/com/technogise/leavemanagement/controllers/HealthControllerTest.java
@@ -1,15 +1,21 @@
 package com.technogise.leavemanagement.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(HealthController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 public class HealthControllerTest {
     @InjectMocks
     private HealthController healthController;
@@ -19,6 +25,9 @@ public class HealthControllerTest {
 
     @Test
     public void Should_ReturnOkStatus_When_EndpointIsAccessed() throws Exception {
+        
+        mockMvc = MockMvcBuilders.standaloneSetup(healthController).build();
+        
         mockMvc.perform(get("/health-check"))
                 .andExpect(status().isOk());
     }
